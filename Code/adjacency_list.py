6e218ffa49ec9712bbec9218af12847eac8a9311
Ansel Bridgewater <iamansel@gmail.com>
2019-07-09 16:17:19 -0700

    Fixed some bugs and refactored

--- Code/adjacency_list.py
+++ Code/adjacency_list.py
@@ -5,33 +5,33 @@ class Graph(object):
 
     def __init__(self):
         """Initialize a graph implemented as an adjacency list."""
-        self.data = {}
+        self.graph = {}
 
     def __repr__(self):
         """Return a string representation of this graph."""
-        return 'Graph({!r})'.format(self.data)
+        return 'Graph({!r})'.format(self.graph)
 
     @property
     def size(self):
         """Return the size of the graph."""
-        return len(self.data)
+        return len(self.graph)
 
     def add_vertex(self, vert):
         """Add a vertex to the graph."""
-        if self.data[vert]:
+        if vert in self.graph:
             raise KeyError("{} is already in the Graph".format(vert))
-        self.data[vert] = []
+        self.graph[vert] = []
 
     def add_edge(self, from_vert, to_vert):
         """Add a new, directed edge to the graph that connects
         two vertices."""
-        if not self.data[from_vert]:
+        if from_vert not in self.graph:
             self.add_vertex(from_vert)
 
-        if not self.data[to_vert]:
+        if to_vert not in self.graph:
             self.add_vertex(to_vert)
 
-        self.data[from_vert].append(to_vert)
+        self.graph[from_vert].append(to_vert)
 
     def add_weighted_edge(self, from_vert, to_vert, weight):
         """Add a new, weighted, directed edge to the graph that connects
@@ -42,17 +42,17 @@ class Graph(object):
     def has_vertex(self, vert_key):
         """Find the vertex in the graph named vert_key. If it exists, return
         True, else return False."""
-        if not self.data[vert_key]:
+        if vert_key not in self.graph:
             return False
         return True
 
     def get_vertices(self):
         """Return the list of all vertices in the graph."""
-        return [vertex for vertex in self.data.keys()]
+        return [vertex for vertex in self.graph.keys()]
 
     def get_neighbors(self, from_vert):
         """List all vertices y such that there is an edge from the
         vertex from_vert to the vertex y."""
-        if not self.data[from_vert]:
+        if from_vert not in self.graph:
             raise KeyError("{} is not in the Graph".format(from_vert))
-        return self.data[from_vert]
+        return self.graph[from_vert]
