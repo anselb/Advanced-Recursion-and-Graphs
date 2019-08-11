fb7fcc65aa151b90258ee650ca80d88921ce680b
Ansel Bridgewater <iamansel@gmail.com>
2019-08-11 16:38:19 -0700

    Finished challenge 5

--- Code/challenge_5/challenge_5.py
+++ Code/challenge_5/challenge_5.py
@@ -4,28 +4,15 @@ import sys
 
 def main():
     """Run the challenge with a passed in graph file name."""
-    if len(sys.argv) < 4:
-        print("Please enter a file name, a start vertex, and an end vertex")
+    if len(sys.argv) < 2:
+        print("Please enter a file name.")
     graph_file = sys.argv[1]
-    start_vert = int(sys.argv[2])
-    end_vert = int(sys.argv[3])
 
     graph = Graph()
     graph.make_graph_from_file(graph_file)
-    path = graph.find_path(start_vert, end_vert)
+    is_eulerian = graph.is_eulerian()
 
-    if path is None:
-        print(f"There exists a path between \
-vertex {start_vert} and {end_vert}: FALSE")
-        print(f"Vertices in the path: {path}")
-        return
-
-    for index in range(len(path)):
-        path[index] = path[index].id
-
-    print(f"There exists a path between \
-vertex {start_vert} and {end_vert}: TRUE")
-    print(f"Vertices in the path: {path}")
+    print(f"This graph is Eulerian: {is_eulerian}")
 
 
 if __name__ == '__main__':
